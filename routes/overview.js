839e91ddf4a92a758723d78f418b71f1941bebac
Gregor Richardson <gregor.richardson@gmail.com>
2018-03-14 06:19:37 -0700

    ovverview working with static snapshots

--- routes/overview.js
+++ routes/overview.js
@@ -26,8 +26,38 @@ router.get('/', mid.requiresLogin, (req, res, next) => {
     //     data.snapshots.push(snap)
     // }))
     // data.snapshots = data.user.info.snapshots;
+    
     data.snapshots = [
-        {"name" : "bob"}
+        {
+          "cryptoCount" : 1,
+          "cryptoGains" : 25736.54,
+          "cryptoGrowth" : 3.8585517241379312,
+          "cryptoValue" : 32406.54,
+          "date" : "01/13/2018",
+          "portfolioGains" : 32198.3,
+          "portfolioGrowth" : 2.221921402644542,
+          "portfolioValue" : 42693.29999999999,
+          "stockCount" : 5,
+          "stockGains" : 6461.76,
+          "stockGrowth" : 1.8945953383458647,
+          "stockValue" : 10286.76,
+          "unix" : 1515993998141
+        },
+        {
+            "cryptoCount" : 1,
+            "cryptoGains" : 23806.839999999997,
+            "cryptoGrowth" : 356.92413793103447,
+            "cryptoValue" : 30476.839999999997,
+            "date" : "01/15/2018",
+            "portfolioGains" : 29579.329999999998,
+            "portfolioGrowth" : 220.1322989740361,
+            "portfolioValue" : 39774.329999999994,
+            "stockCount" : 6,
+            "stockGains" : 5772.49,
+            "stockGrowth" : 197.3336591478697,
+            "stockValue" : 9297.49,
+            "unix" : 1515993868864
+          }
     ]
     
     // data.assets = JSON.parse(data.user.assets);
